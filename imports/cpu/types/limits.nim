b88e24a1246953e07424184f6649a755df9efaa4
yisitluke-null <yisitluke@gmail.com>
2026-05-24 23:19:36 -0400

    Issue with limits.nim

--- imports/cpu/types/limits.nim
+++ imports/cpu/types/limits.nim
@@ -1,4 +1,4 @@
 const LIM_MINIMUM* = 0 # FatalCrash if exceeded
-const LIM_MAXIMUM* = 2^16 # FatalCrash if exceeded
+const LIM_MAXIMUM* = 65536 # FatalCrash if exceeded
 const LIM_DGRZONETHRESHOLD* = 3
 const LIM_DGRZONE* = LIM_MAXIMUM div LIM_DGRZONETHRESHOLD # used to determine if a number is getting too high
