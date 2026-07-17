3aa7404a7c4cb8dd9e496cf2abfbeef8682b732a
yizluck <yizluck@gmail.com>
2026-07-17 16:49:41 -0400

    Better build script

--- build.sh
+++ build.sh
@@ -7,21 +7,45 @@ PROJECT_NAME="nacto"
 MAIN_FILE="main.nim"
 OUTPUT_DIR="bin"
 
-echo "What are you compiling for?"
-echo "1. Linux"
-echo "2. Windows"
-read -rp "> " opt
+TARGET=""
 
-case "$opt" in
-    1)
-        TARGET="linux"
-        NIM_OS=""
-        EXT=""
+# Helpers
+set-linux() {
+    TARGET="linux"
+    NIM_OS=""
+    EXT=""
+}
+
+set-windows() {
+    TARGET="windows"
+    NIM_OS="-d:mingw"
+    EXT=".exe"
+}
+
+build() {
+    mkdir -p "$OUTPUT_DIR"
+
+    echo "Compiling $PROJECT_NAME for $TARGET..."
+    nim c $NIM_OS --out:"$OUTPUT_DIR/$PROJECT_NAME$EXT" --path:"imports" --hints:on -d:$BUILD_MODE --warnings:on "$MAIN_FILE"
+
+    cp -r "initfs" "$OUTPUT_DIR/initfs"
+
+    echo "Build complete: $OUTPUT_DIR/$PROJECT_NAME$EXT"
+
+    exit 0
+}
+
+# Non-interactive
+case "${1:-}" in
+    "linux")
+        set-linux
+        build
         ;;
-    2)
-        TARGET="windows"
-        NIM_OS="-d:mingw"
-        EXT=".exe"
+    "windows")
+        set-windows
+        build
+        ;;
+    "")
         ;;
     *)
         echo "Invalid option."
@@ -29,11 +53,23 @@ case "$opt" in
         ;;
 esac
 
-mkdir -p "$OUTPUT_DIR"
+# Interactive
+echo "What are you compiling for?"
+echo "1. Linux"
+echo "2. Windows"
+read -rp "> " opt
 
-echo "Compiling $PROJECT_NAME for $TARGET..."
-nim c $NIM_OS --out:"$OUTPUT_DIR/$PROJECT_NAME$EXT" --path:"imports" --hints:on -d:$BUILD_MODE --warnings:on "$MAIN_FILE"
-
-cp -r "initramfs" "$OUTPUT_DIR/initramfs"
-
-echo "Build complete: $OUTPUT_DIR/$PROJECT_NAME$EXT"
+case "$opt" in
+    1)
+        set-linux
+        build
+        ;;
+    2)
+        set-windows
+        build
+        ;;
+    *)
+        echo "Invalid option."
+        exit 1
+        ;;
+esac
